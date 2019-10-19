676781ce2c0af09e6e7691fcbbec9299420ee769
Mark Silberberg <mark.silberberg20@gmail.com>
2019-10-19 10:31:48 -0500

    "Added Further Lessons to the Housing Example"

--- Chapter_3/MNIST.py
+++ Chapter_3/MNIST.py
@@ -2,3 +2,10 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', version=1)
 print(mnist.keys())
+
+#Datasets loaded by Scikit-Learn generally have a similar dictionary structure including:
+# 1.) A DESCR key describing the dataset
+# 2.) A data key containing an array with one row per instance and one column per feature
+# 3.) A target key containing an array with the labels
+
+#Recall: fetch is used to pull down a program or dataset
